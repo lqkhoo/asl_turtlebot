58ee6bb4799de891cb193e78871acadfd80d2460
lqkhoo <changtau2005@gmail.com>
2020-10-20 04:14:56 +0800

    Section 5 complete

--- scripts/planners/path_smoother.py
+++ scripts/planners/path_smoother.py
@@ -19,7 +19,46 @@ def compute_smoothed_traj(path, V_des, alpha, dt):
     Hint: Use splrep and splev from scipy.interpolate
     """
     ########## Code starts here ##########
-    raise NotImplementedError # REPLACE THIS FUNCTION WITH YOUR IMPLEMENTATION
+    tups = zip(*path)
+    x = np.array(tups[0], dtype=np.float)
+    y = np.array(tups[1], dtype=np.float)
+    N = x.shape[0]
+
+    dx = np.diff(x, n=1)
+    dy = np.diff(y, n=1)
+
+    l2 = np.sqrt(dx*dx + dy*dy) # l2 distance between each point.
+    dt_ = l2 / V_des            # Time to travel between each segment at nominal speed.
+    t = np.zeros(N)
+    t[1:] = np.cumsum(dt_)      # Time at each point if travelling at nominal velocity
+    t_f = t[-1]                 # Final time
+    
+    tck, u = scipy.interpolate.splprep([x, y], k=3, s=alpha) # Fit the spline
+
+    subsample = True
+    if subsample:
+        N_SAMPLES = 512
+        u = np.linspace(0, 1, N_SAMPLES)
+
+    ts = u * t_f
+    # Hence
+    du_dt = 1 / t_f
+    d2u_dt2 = 0
+
+    xi, yi = scipy.interpolate.splev(u, tck, der=0)
+    thetai = np.arctan2(yi, xi)
+    xi_dot, yi_dot = scipy.interpolate.splev(u, tck, der=1)
+    xi_dot = xi_dot * du_dt
+    yi_dot = yi_dot * du_dt
+
+    xi_ddot, yi_ddot = scipy.interpolate.splev(u, tck, der=2)
+    xi_ddot = xi_ddot * d2u_dt2
+    yi_ddot = yi_ddot * d2u_dt2
+    
+    traj_smoothed = np.array([xi, yi, thetai, xi_dot, yi_dot, xi_ddot, yi_ddot]).T
+    
+    t_smoothed = ts
+
     ########## Code ends here ##########
 
     return traj_smoothed, t_smoothed
